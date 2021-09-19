87059d2f0bfdc2d02f1c488191649149fc47d98e
DanielVillatoro <danielvillatoroc@gmail.com>
2021-09-18 20:33:34 -0600

    Firts Commit

---
+++ src/ProblemaMochilaScreen.js
@@ -0,0 +1,10 @@
+import React, {  } from 'react';
+// import { useForm } from './hooks/useForm';
+// import axios from 'axios';
+
+export const ProblemaMochilaScreen = () => {
+    return (<div className="container">
+        <h1>Problema de la mochila</h1>
+        <hr />
+    </div>)
+}
